320fa99df8ec7da78723355707785ed4885f6387
nehallxx <nehalkhaled0@gmail.com>
2025-05-01 13:36:51 +0300

    feat(paylink): after changes made to add paylink settings

--- paylink/hooks.py
+++ paylink/hooks.py
@@ -6,36 +6,6 @@ app_email = "mostafa.a.yasin@gmail.com"
 app_license = "mit"
 
 
-fixtures = [
-    {
-        "dt": "Custom Field",
-        "filters": [
-            [
-                "name",
-                "in",
-                [
-                    "Sales Invoice-custom_paylink_details",
-                    "ales Invoice-custom_paylink_payment_link",
-                    "Sales Invoice-custom_paylink_transaction_id",
-                    "Sales Invoice-custom_column_break_pcmhh",
-                    "Sales Invoice-custom_paylink_payment_status",
-                    "Sales Invoice-custom_paylink"
-                ],
-            ]
-        ],
-    },
-    
-    {
-        "dt": "DocType",
-        "filters": [
-            [
-                "name",
-                "in",
-                ["Paylink Settings"],
-            ]
-        ],
-    }
-]
 
 
 #------------------------Actions------------------------#
